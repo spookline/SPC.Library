355700459273816d0b218e122b62128729dd0176
ReaperMaga <reapermaga@gmail.com>
2026-05-13 04:10:24 +0300

    feat: Add SpookManagerNetworkBehaviour script and update package version to 0.1.3

---
+++ SPC.FishNet/Runtime/Scripts/SpookManagerNetworkBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Spookline.SPC.FishNet {
+    public class SpookManagerNetworkBehaviour<TSelf> : SpookNetworkBehaviour<TSelf>
+        where TSelf : SpookManagerNetworkBehaviour<TSelf> {
+
+        public static TSelf Instance { get; private set; }
+
+        public static bool HasInstance => Instance;
+
+        protected virtual void Awake() {
+            if (HasInstance) {
+                Debug.LogError($"Instance of {typeof(TSelf).Name} already exists. Destroying this instance.");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = (TSelf)this;
+        }
+
+    }
+}
