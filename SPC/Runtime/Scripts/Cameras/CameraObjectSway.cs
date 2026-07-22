19c2097b88ec6155b48f8b590600ce89b464faf9
ReaperMaga <reapermaga@gmail.com>
2026-07-22 16:56:07 +0300

    feat: Add CameraObjectSway system with presets and sway logic
    Introduced a new CameraObjectSway system to enable object sway based on camera movement. Includes a `CameraObjectSway` script and a configurable `CameraObjectSwayPreset` scriptable object to define sway parameters. Added related assets and updated character controller to integrate the new system.

---
+++ SPC/Runtime/Scripts/Cameras/CameraObjectSway.cs
@@ -0,0 +1,47 @@
+﻿using Sirenix.OdinInspector;
+using Spookline.SPC.Ext;
+using UnityEngine;
+
+namespace Spookline.SPC.Cameras {
+    [DefaultExecutionOrder(1000)]
+    public class CameraObjectSway : SpookManagerBehaviour<CameraObjectSway> {
+
+        [Required]
+        public new Transform camera;
+
+        public CameraObjectSwayPreset preset;
+
+        private Vector3 _initialPosition;
+        private Vector3 _currentPosition;
+        private Vector3 _lastCameraEuler;
+
+        private Vector3 _targetPosition;
+
+        private void Awake() {
+            _initialPosition = transform.localPosition;
+            _currentPosition = _initialPosition;
+        }
+
+        private void LateUpdate() {
+            if (!camera) return;
+            var cameraEuler = camera.localEulerAngles;
+            var deltaX = Mathf.DeltaAngle(_lastCameraEuler.x, cameraEuler.x);
+            var deltaY = Mathf.DeltaAngle(_lastCameraEuler.y, cameraEuler.y);
+            var movement = new Vector3(-deltaY, -deltaX, 0) * preset.rotationSwayAmount;
+
+            var swayX = preset.useX ? Mathf.Sin(Time.time * preset.x.speed) * preset.x.amount : 0f;
+            var swayY = preset.useY ? Mathf.Abs(Mathf.Sin(Time.time * preset.y.speed)) * preset.y.amount : 0f;
+            movement.x += swayX;
+            movement.y += swayY;
+
+            movement = Vector3.ClampMagnitude(movement, preset.maxSwayAmount);
+            _targetPosition = _initialPosition + movement;
+            
+            var time =  1f - Mathf.Exp(-preset.smoothSpeed * Time.deltaTime);
+            _currentPosition = Vector3.Lerp(_currentPosition, _targetPosition, time);
+            transform.localPosition = _currentPosition;
+            _lastCameraEuler = cameraEuler;
+        }
+
+    }
+}
---
+++ SPC/Runtime/Scripts/Cameras/CameraObjectSwayPreset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Spookline.SPC.Cameras {
+    [CreateAssetMenu(
+        fileName = "Camera Object Sway Preset", menuName = "Camera/Camera Object Sway Preset")]
+    public class CameraObjectSwayPreset : SerializedScriptableObject {
+        
+        public float rotationSwayAmount = 0.05f;
+        public float maxSwayAmount = 0.1f;
+        public float smoothSpeed = 8f;
+        
+        [ToggleGroup("useX", groupTitle: "X")]
+        public bool useX = true;
+        [ToggleGroup("useX"), InlineProperty, HideLabel]
+        public CameraObjectSwayModifier x;
+        [ToggleGroup("useY", groupTitle: "Y")]
+        public bool useY;
+        [ToggleGroup("useY"), InlineProperty, HideLabel]
+        public CameraObjectSwayModifier y;
+
+    }
+
+    [Serializable]
+    public class CameraObjectSwayModifier {
+        
+        public float amount = 0.02f;
+        public float speed = 8f;
+
+    }
+}
